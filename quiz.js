f3465181593e1d47c64f2bd88168970e598f9e15
Aakash Kumawat <115066628+aakashkumawat091@users.noreply.github.com>
2023-03-22 12:59:46 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,6 @@
+let startQuize = document.querySelector("button")
+let screen1 = document.querySelector("#screen1")
+let screen2 = document.querySelector("#screen2")
+let timer = document.querySelector(".timer")
+let timer = 5;
+let question = [5] 
